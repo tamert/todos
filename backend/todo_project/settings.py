4ceeb3ea146d9839df7b1eb6a3ea250f60bd6ffe
Tamer Ağaoğlu <farerock@gmail.com>
2019-10-16 09:46:07 +0300

    mysql selected

--- backend/todo_project/settings.py
+++ backend/todo_project/settings.py
@@ -97,8 +97,12 @@ WSGI_APPLICATION = 'todo_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'todo',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '/Applications/MAMP/tmp/mysql/mysql.sock',
+        'PORT': '3306'
     }
 }
 
@@ -133,7 +137,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
